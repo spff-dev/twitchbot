c8facc60307fe08c780c62f5d87b2b0f1e57f89c
spff-dev <116954571+spff-dev@users.noreply.github.com>
2025-09-12 11:41:56 +0100

    cleanup: remove legacy static command links (config-only)

--- commands/links.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'links',
-  description: 'Send links urls',
-  permission: 'everyone',
-  cooldownSec: 30,
-  async run(ctx) {
-    return ctx.say("Spiff's links: https://spiff.gg/");
-  }
-};
