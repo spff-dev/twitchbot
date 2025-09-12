f730014edb22355c2e8852adaad36c0569846a84
spff-dev <116954571+spff-dev@users.noreply.github.com>
2025-09-12 11:43:18 +0100

    cleanup: remove legacy static command dbd (config-only)

--- commands/dbd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'dbd',
-  description: 'Send DBD ID',
-  permission: 'everyone',
-  cooldownSec: 0,
-  async run(ctx) {
-    return ctx.replyThread("ℹ️ Spiff's DBD ID is: Spiff#d0d5 or SPIFFgg#d0d5 - depending on his name in-game.");
-  }
-};
