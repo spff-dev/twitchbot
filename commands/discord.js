9a7e781946d392636a43ce28e824892003f5ddc0
spff-dev <116954571+spff-dev@users.noreply.github.com>
2025-09-12 06:32:29 +0100

    cleanup: remove legacy static command discord (config-only)

--- commands/discord.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'discord',
-  description: 'Send link to the Discord',
-  permission: 'everyone',
-  cooldownSec: 30,
-  async run(ctx) {
-    return ctx.say("Join the Spiffcord! https://discord.gg/x65rDmMycn");
-  }
-};
