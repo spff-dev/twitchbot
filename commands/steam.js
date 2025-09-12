9ec6662fbd9b176c8c4bd18e805d506b34d5c7ba
spff-dev <116954571+spff-dev@users.noreply.github.com>
2025-09-12 11:55:37 +0100

    cleanup: remove legacy static command steam (config-only)

--- commands/steam.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'steam',
-  description: 'Send Steam profile url',
-  permission: 'everyone',
-  cooldownSec: 15,
-  async run(ctx) {
-    return ctx.replyThread("Spiff's Steam profile is: https://steamcommunity.com/id/spiffgg/");
-  }
-};
