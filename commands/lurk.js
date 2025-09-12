2907599da3153c1f755c2287ad243c02d7517bc1
spff-dev <116954571+spff-dev@users.noreply.github.com>
2025-09-12 11:11:46 +0100

    cleanup: remove legacy static command lurk (config-only)

--- commands/lurk.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'lurk',
-  description: 'Send a friendly lurk message',
-  permission: 'everyone',
-  cooldownSec: 0,
-  async run(ctx) {
-    return ctx.replyThread('Enjoy your lurk! Spiff appreciates you being here!');
-  }
-};
